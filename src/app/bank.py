238548fd1e5bcb1eeea6a9503250e6e99e8fe1cb
Tina <tina@siebs.de>
2019-02-05 17:06:32 +0100

    Bankaccount

---
+++ src/app/bank.py
@@ -0,0 +1,29 @@
+class Bank:
+    def __init__(self, name):
+        self.name = name
+        self.accounts = []
+        self.transactions = []
+
+    def open_account(self, account):
+        self.accounts.append(account)
+        return account
+
+    def add_transaction(self, *,sender, recipient, subject, amount):
+        assert amount >0, 'Amount has to be greater than 0'
+        transaction = {
+            'sender': sender,
+            'recipient': recipient,
+            'subject': subject,
+            'amount': amount
+        }
+        self.transactions.append(transaction)
+        return transaction
+
+
+
+
+
+
+
+
+
